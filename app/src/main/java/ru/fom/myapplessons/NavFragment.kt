b0ed51ab5f20be116f4eeb9b18d6c61bab646cf9
Alex <temp125@br.ru>
2020-11-02 11:27:24 +0700

    two screens

---
+++ app/src/main/java/ru/fom/myapplessons/NavFragment.kt
@@ -0,0 +1,34 @@
+package ru.fom.myapplessons
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_nav.*
+
+class NavFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nav, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_navFragment_to_NAvkFragment2)
+        }
+    }
+
+}
