2846c8bb237e2364bbacba9161f58d665691e378
Caplin Aleksei <work_web@list.ru>
2020-11-22 20:26:16 +0700

    add interseptor client

--- app/src/main/java/ru/fom/myapplessons/net/RetrofitApiClient.kt
+++ app/src/main/java/ru/fom/myapplessons/net/RetrofitApiClient.kt
@@ -1,6 +1,7 @@
 package ru.fom.myapplessons.net
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,8 +9,16 @@ object RetrofitApiClient {
 
     private var retrofit: Retrofit? = null
 
+    private val interceptor = run {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.apply {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
     private val client: OkHttpClient = OkHttpClient
         .Builder()
+        .addInterceptor(interceptor)
         .build()
 
     fun getClient(baseUrl: String): Retrofit {
