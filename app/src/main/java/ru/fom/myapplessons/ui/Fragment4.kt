7b374ce96a643c14f0ad1ce3a9131ddfa727ed1d
Alex <temp125@br.ru>
2021-01-11 14:07:37 +0700

    начал делать боковое меню

---
+++ app/src/main/java/ru/fom/myapplessons/ui/Fragment4.kt
@@ -0,0 +1,24 @@
+package ru.fom.myapplessons.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.fom.myapplessons.R
+
+class Fragment4 : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_4, container, false)
+    }
+}
